d9f959b247729c21e2f419786f3b5b66861b388d
Deivid de Oliveira Souza <deivid@totalseg.com.br>
2023-01-04 12:35:32 -0300

    continuação da aula

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 const app = express();
 
+app.use(express.json());
+
 const livros = [
     {id:1,titulo:"Senhor dos Anéis"},
     {id:2,titulo:"O iluminado"},
@@ -13,5 +15,30 @@ app.get("/",function(req,resp){
 app.get("/livros",function(req,resp){
     resp.status(200).json(livros);
 });
+
+app.get("/livros/:id",(req,resp)=>{
+    let index = buscaLivro(req.params.id);
+    resp.json(livros[index]);
+});
+
+app.post("/livros",(req,resp)=>{
+    livros.push(req.body);
+    resp.status(201).send("Livro Cadastrado com sucesso");
+});
+app.put("/livros/:id",(req,resp)=>{
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    resp.json(livros);
+});
+app.delete("/livros/:id",(req,resp)=>{
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    livros.splice(index,1);
+    resp.send(`Livro ${id} REmovido com sucesso`);
+});
+
+function buscaLivro(id){
+    return livros.findIndex(livro=>livro.id == id);
+}
 
 export default app;
